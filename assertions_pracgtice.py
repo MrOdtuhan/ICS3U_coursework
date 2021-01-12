e83a2de75e37ba0f713f58cb5f0995f677517b5e
MrOdtuhan <73952447+MrOdtuhan@users.noreply.github.com>
2021-01-11 21:36:15 -0500

    Create assertions_pracgtice.py

---
+++ assertions_pracgtice.py
@@ -0,0 +1,8 @@
+# Take Codingbat tests and convert them to assert statements.
+
+def test_sleep_in():
+    assert sleep_in(False, False) == True
+
+
+def test_monkey_trouble():
+    assert monkey_trouble(True, True) == True
